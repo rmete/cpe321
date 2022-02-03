8b51c8d4c0673b3a13524b5825cfc5b3d268b636
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-03 10:42:06 -0800

    base aes_modes

--- BlockCiphers/AES_modes.py
+++ BlockCiphers/AES_modes.py
@@ -1,1 +1,9 @@
-import
+from Crypto.Cipher import AES
+import sys
+
+def main(file_path):
+    cipher = AES.new([key], [mode])
+    print("main")
+
+if __name__ == "__main__":
+    main(sys.argv[0])
