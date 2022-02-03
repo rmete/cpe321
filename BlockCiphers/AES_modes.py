d45b5860ff02935649cec9f825bb27f415989562
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-03 13:02:15 -0800

    Added padding Added get blocks method

--- BlockCiphers/AES_modes.py
+++ BlockCiphers/AES_modes.py
@@ -5,14 +5,25 @@ import random
 
 def main(in_file_path):
     with open(in_file_path, mode='rb') as in_file:
-        key = get_random_bytes(16)
-        iv = get_random_bytes(16)
-        cipher = AES.new(key, AES.MODE_ECB)
-        out_file = CBC_encrypt(in_file_path, get_blocks(in_fie), cipher, iv)
+        file_bytes = in_file.read()
+        blocks = get_blocks(file_bytes)
+        print(blocks)
+        # key = get_random_bytes(16)
+        # iv = get_random_bytes(16)
+        # cipher = AES.new(key, AES.MODE_ECB)
+        # out_file = CBC_encrypt(in_file_path, get_blocks(in_file), cipher, iv)
 
 
-def get_blocks(bytes):
-    pass
+def pkcs7_pad(to_pad, m):
+    if len(to_pad) % m != 0:
+        pad_with = (m * (len(to_pad) // m + 1)) - len(to_pad)
+        to_pad = to_pad + bytes([pad_with] * pad_with)
+    return to_pad
+
+def get_blocks(file_as_bytes):
+    file_as_bytes = pkcs7_pad(file_as_bytes, 16)
+    blocks = [bytes(file_as_bytes[x - 16: x]) for x in range(16, len(file_as_bytes) + 1, 16)]
+    return blocks
 
 
 def xor_bytes(a, b):
