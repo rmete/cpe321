7c8338f85f53c5af180efb10c067c9f68475ad3f
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-04 11:35:03 -0800

    Submit and verify

--- BlockCiphers/task2.py
+++ BlockCiphers/task2.py
@@ -17,9 +17,9 @@ def submit(usr_input, key, iv):
     input_bytes = bytearray()
     for char_byte in usr_input:
         if char_byte == ';':
-            input_bytes.append(colon)
+            input_bytes += colon
         elif char_byte == '=':
-            input_bytes.append(equals)
+            input_bytes += equals
         else:
             input_bytes += bytes(char_byte, "ascii")
 
@@ -31,7 +31,13 @@ def submit(usr_input, key, iv):
 def verify(input_bytes, key, iv):
     input_blocks = AES_modes.get_blocks(input_bytes)
     decrypted = AES_modes.CBC_decrypt(input_blocks, key, iv)
-    return decrypted
+    decrypted_str = decrypted.decode("ascii")
+    print(decrypted_str)
+    look_for = "%3badmin%3dtrue%3b"
+    for idx in range(len(look_for) - 1, len(decrypted_str)):
+        if decrypted_str[idx - len(look_for):idx] == look_for:
+            return True
+    return False
 
 
 
