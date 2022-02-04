66f3edae50cff5b97f7b96c652a783d11eebfcb7
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-04 11:57:23 -0800

    succesful CBC byte flipping attack

--- BlockCiphers/task2.py
+++ BlockCiphers/task2.py
@@ -2,13 +2,26 @@ import AES_modes
 from Crypto.Random import get_random_bytes
 import sys
 
+
 def main(user_input):
     key = get_random_bytes(16)
     iv = get_random_bytes(16)
 
+    # when the input is too short there is only one block, so the attack isn't possible
+    user_input = "some non-malicious user input, that has at least 256 bytes"
+    user_input_bytes = bytes(user_input, "ascii")
     encrypted_bytes = submit(user_input, key, iv)
-    print(verify(encrypted_bytes, key, iv))
 
+    #
+    #CBC byte flipping attack
+    #
+    we_want = bytes(";admin=true;____", "ascii")
+    decoded_not_xored = AES_modes.xor_bytes(encrypted_bytes[:16], user_input_bytes[16:32]) #second block so we can modify the first
+    malicious_cipher_text = AES_modes.xor_bytes(decoded_not_xored, we_want)
+    #do modification
+    encrypted_bytes = malicious_cipher_text + encrypted_bytes[16:]
+
+    print(verify(encrypted_bytes, key, iv))
 
 
 def submit(usr_input, key, iv):
@@ -31,15 +44,12 @@ def submit(usr_input, key, iv):
 def verify(input_bytes, key, iv):
     input_blocks = AES_modes.get_blocks(input_bytes)
     decrypted = AES_modes.CBC_decrypt(input_blocks, key, iv)
-    decrypted_str = decrypted.decode("ascii")
-    print(decrypted_str)
-    look_for = "%3badmin%3dtrue%3b"
-    for idx in range(len(look_for) - 1, len(decrypted_str)):
-        if decrypted_str[idx - len(look_for):idx] == look_for:
+    print(decrypted)
+    look_for = bytes(";admin=true;", "ascii")
+    for idx in range(len(look_for) - 1, len(decrypted)):
+        if decrypted[idx - len(look_for):idx] == look_for:
             return True
     return False
-
-
 
 
 if __name__ == "__main__":
