1bbad425f66f8cacc6856fd2a2562e1f3cb4c2cb
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-21 12:54:22 -0800

    task2

--- Auth_Hashing/task2.py
+++ Auth_Hashing/task2.py
@@ -1,9 +1,27 @@
 from bcrypt import hashpw
 from bcrypt import checkpw
+from nltk.corpus import words
+from time import process_time
+import threading
+
+
+def brute_force(name, hashed_pw):
+    start = process_time()
+    for guess in words:
+        if checkpw(guess, hashed_pw):
+            print(f"{name} found in: {process_time() - start}")
+            return
 
 
 def main():
-    pass
+    threads = []
+    with open("shadow.txt", 'rb') as to_crack:
+        line = to_crack.readline().split(':')
+        print(line)
+        threads.append(threading.Thread(brute_force, (line[0], line[1])))
+        threads[-1].run()
+
+
 
 
 if __name__ == '__main__':
