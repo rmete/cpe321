a72128d5f3fe9554060a76b2c862d6c2bc3271e5
Caleb Dennis <caleb.dennis429@gmail.com>
2022-02-21 14:09:39 -0800

    threading baby

--- Auth_Hashing/task2.py
+++ Auth_Hashing/task2.py
@@ -5,21 +5,37 @@ from time import process_time
 import threading
 
 
-def brute_force(name, hashed_pw):
+def brute_force(name, hashed_pw, words):
     start = process_time()
-    for guess in words.words():
-        if checkpw(guess, hashed_pw):
-            print(f"{name} found in: {process_time() - start}")
-            return
+    print(f"{name} {hashed_pw}")
+    for guess in words:
+        if len(guess) > 5 and len(guess) < 11:
+            if checkpw(bytes(guess, 'ascii'), bytes(hashed_pw, 'ascii')):
+                print(f"{name} found in: {process_time() - start}")
+                return
 
 
 def main():
     threads = []
+    name_passwords = []
     with open("shadow.txt", 'r') as to_crack:
-        line = to_crack.readline().split(':')
-        print(line)
-        threads.append(threading.Thread(target=brute_force, args=(line[0], line[1])))
-        threads[-1].run()
+        name_passwords = [line.strip().split(':') for line in to_crack.readlines()]
+
+    for line in name_passwords:
+        threads.append(threading.Thread(target=brute_force, args=(line[0], line[1], words.words()), daemon=True))
+        threads[-1].start()
+
+    for t in threads:
+        t.join()
+
+    # thread_alive = True
+    # while thread_alive:
+    #     thread_alive = False
+    #     for t in threads:
+    #         if t.is_alive() == True: thread_alive = True
+
+    print("done")
+
 
 
 
